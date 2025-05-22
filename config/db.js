55a78b96b69b722c4f8fc3f17909bc5198377575
PC-GEM <godwinmangai3@gmail.com>
2025-05-22 13:13:50 +0100

    auth and database

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.LOCAL_MONGO);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('Error:', error.message);
