9c14b187d2a32063d0647e8dc1369c96f2579b3c
PC-GEM <godwinmangai3@gmail.com>
2025-05-24 23:30:27 +0100

    prembly vnin

--- utilities/quickVerify.js
+++ utilities/quickVerify.js
@@ -2,18 +2,12 @@ require('dotenv').config();
 const axios = require('axios');
 
 const checkNIN = async (nin) => {
-  const base_url = process.env.PREMBLY_BASE_URL_SANDBOX;
-  console.log('first step');
+  const base_url = process.env.PREMBLY_BASE_URL;
 
   try {
-    console.log('Sending request to:', `${base_url}/api/v1/biometrics/merchant/data/verification/nin`);
-    console.log('Using headers:', {
-      'x-api-key': process.env.PREMBLY_API_KEY,
-      'app-id': process.env.PREMBLY_APP_ID,
-    });
 
     const response = await axios.post(
-      `${base_url}/api/v1/kyc/nin`,
+      `${base_url}/identitypass/verification/vnin`,
       { number: nin },
       {
         headers: {
@@ -24,19 +18,18 @@ const checkNIN = async (nin) => {
       }
     );
 
-    console.log('Response from Prembly:', response.data);
-
     const result = response.data;
-
-    if (!result?.status || result.status !== 'success') {
-      throw new Error('NIN verification failed');
+    console.log('the result is', result);
+    if (!result?.status || result.verification?.status !== 'VERIFIED') {
+      throw new Error('❌ NIN verification failed');
     }
 
     return result.data;
 
   } catch (error) {
-    console.error('❌ Error during NIN verification:', error.response?.data || error.message || error);
-    throw new Error('NIN verification failed');
+    console.log('the error is', error)
+    const errData = error.response?.data || error.message;
+    throw new Error(`❌ NIN verification error: ${JSON.stringify(errData)}`);
   }
 };
 
