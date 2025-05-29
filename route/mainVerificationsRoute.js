19547791f21ad9bb709b94f855b1dd5fc45da84f
PC-GEM <godwinmangai3@gmail.com>
2025-05-29 16:58:30 +0100

    bvn update

--- route/mainVerificationsRoute.js
+++ route/mainVerificationsRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {verifyNin} = require('../controller/ninController');
+const {verifyBvn} = require('../controller/bvnController');
 
 router.post('/nin', verifyNin);
+router.post('/bvn', verifyBvn);
 
 module.exports = router;
