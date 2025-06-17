df13034db1cd8fcad564d5c2f46df189e3bf0d77
PC-GEM <godwinmangai3@gmail.com>
2025-06-17 16:48:57 +0100

    delete user

--- controller/adminController.js
+++ controller/adminController.js
@@ -51,10 +51,24 @@ const updateUser = async (req, res) => {
   }
 };
 
+const deleteUser = async (req, res) => {
+  const { userId } = req.body;
+
+  try {
+    const user = await UserModel.findByIdAndDelete(userId);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    console.error('Delete user error:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
 
 module.exports = {
   getAllUsers,
   getAllTransactions,
-  updateUser
+  updateUser,
+  deleteUser
 };
--- route/adminRoute.js
+++ route/adminRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getAllTransactions, updateUser } = require('../controller/adminController');
+const { getAllUsers, getAllTransactions, updateUser, deleteUser } = require('../controller/adminController');
 const { verifyAdmin } = require('../middleware/adminMiddleware');
 
 router.get('/users/:adminUserId', verifyAdmin, getAllUsers);
 router.get('/transactions/:adminUserId', verifyAdmin, getAllTransactions);
 router.patch('/updateUser/:adminUserId', updateUser);
+router.delete('/delete/:adminUserId', deleteUser );
 
 module.exports = router;
