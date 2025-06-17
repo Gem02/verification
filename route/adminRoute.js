0a04735e9a0befd0ce4c04b5fe7e0e89a3186c20
PC-GEM <godwinmangai3@gmail.com>
2025-06-17 16:27:53 +0100

    patch update

--- route/adminRoute.js
+++ route/adminRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, getAllTransactions } = require('../controller/adminController');
+const { getAllUsers, getAllTransactions, updateUser } = require('../controller/adminController');
 const { verifyAdmin } = require('../middleware/adminMiddleware');
 
 router.get('/users/:adminUserId', verifyAdmin, getAllUsers);
 router.get('/transactions/:adminUserId', verifyAdmin, getAllTransactions);
+router.patch('updateUser/:adminUserId', updateUser);
 
 module.exports = router;
